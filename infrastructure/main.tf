082fd0de23aa31c4d9fbba4691b3166bc1397458
coling01 <colin.greenwood@hmcts.net>
2025-11-17 15:32:11 +0000

    chore: re-add tf common_tags

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,13 @@
+locals {
+  tags = (merge(
+    var.common_tags,
+    tomap({
+      "Team Contact" = var.team_contact
+      "Destroy Me"   = var.destroy_me
+    })
+  ))
+}
+
 provider "azurerm" {
   features {}
 }
