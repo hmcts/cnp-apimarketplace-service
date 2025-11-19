eec18fce9b2beb60885afac118d51be2fea8f2de
coling01 <colin.greenwood@hmcts.net>
2025-11-19 17:02:08 +0000

    chore: add debug to SmokeTest

--- src/smokeTest/java/uk/gov/hmcts/apimarketplace/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/apimarketplace/controllers/SampleSmokeTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.apimarketplace.controllers;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.given;
 
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleSmokeTest {
     @Value("${TEST_URL:http://localhost:4550}")
@@ -23,15 +25,20 @@ class SampleSmokeTest {
 
     @Test
     void smokeTest() {
-        Response response = given()
-            .baseUri(testUrl)
-            .contentType(ContentType.JSON)
-            .when()
-            .get()
-            .then()
-            .extract().response();
-
-        Assertions.assertEquals(200, response.statusCode());
-        Assertions.assertTrue(response.asString().startsWith("Welcome"));
+        log.info("SmokeTest hitting endpoint {}", testUrl);
+        try {
+            Response response = given()
+                .baseUri(testUrl)
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .extract().response();
+            log.info("SmokeTest response:{}", response.asString());
+            Assertions.assertEquals(200, response.statusCode());
+            Assertions.assertTrue(response.asString().startsWith("Welcome"));
+        } catch (Exception e) {
+            log.info("SmokeTest exception ", e);
+        }
     }
 }
